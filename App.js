4c98012710508f90095387714e96b8ca7450b65b
Filipi Dantas <127785380+Filipi0@users.noreply.github.com>
2024-11-22 08:03:27 -0300

    FIM

--- App.js
+++ App.js
@@ -1,10 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Image } from 'react-native';
+import { Button } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Image
+        source={require('./assets/Daco_832227.png')}
+        style={{ width: 140, height: 140 }}
+      />
+
+      <Text style={styles.text1}>Cadastre seu jogador</Text>
+
+      <TextInput
+        style={styles.input}
+        placeholder="Nome"
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Idade"
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Numero da camisa"
+      />
+
+      <Button
+        style={styles.button}
+        title='Cadastrar'
+        onPress={() => alert('NOVO CRAQUE NO MENGÃO!!')}
+
+      />
+
       <StatusBar style="auto" />
     </View>
   );
@@ -14,7 +41,29 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+    padding: 50,
     alignItems: 'center',
-    justifyContent: 'center',
+  },
+
+  text1: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    padding: 20,
+    left: 100,
+  },
+
+  input: {
+    height: 40,
+    borderWidth: 1,
+    paddingHorizontal: 10,
+    marginVertical: 10,
+    width: '100%',
+    borderRadius: 16,
+  },
+
+  button: {
+    backgroundColor: 'black',
+    padding: 20,
+    borderRadius: 5,
   },
 });
